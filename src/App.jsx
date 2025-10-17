9a38826c98c5e2e5b767feaefa77e7c9d6f78460
Masum Prodhan <masumbillahprodhan003@gmail.com>
2025-10-18 01:33:49 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,20 @@
+import './App.css'
+import Navbar from './components/Navbar/navbar'
+import Blogs from './components/Blogs/Blogs'
+
+
+function App() {
+  
+
+  return (
+    <>
+     
+      <Navbar></Navbar>
+      <Blogs></Blogs>
+     
+    
+    </>
+  )
+}
+
+export default App
